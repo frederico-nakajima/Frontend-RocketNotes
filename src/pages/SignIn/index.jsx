8496412d2bdba0daf9bd0b648ab4504b31f58133
frederico <fredericonakajimanakajima@gmail.com>
2024-07-25 03:40:58 -0300

    Signin

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -4,11 +4,21 @@ import { Button } from "../../components/Button";
 import { FiMail, FiLock } from "react-icons/fi";
 import  { Link } from 'react-router-dom';
 import { useAuth } from "../../hooks/auth";
+import { useState } from 'react';
+
+
 
 export function SignIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const data = useAuth();
-    console.log("MEU CONTEXTO =>",data);
+
+    const { signIn } = useAuth();
+
+    function handleSignIn(){
+        signIn({ email, password });
+    }
+    
     return(
         <Container>
             <Form>
@@ -21,15 +31,17 @@ export function SignIn(){
                     placeholder = "E-mail"
                     type = "text"
                     icon = {FiMail}
+                    onChange = {e => setEmail(e.target.value)}
                 />
 
                 <Input 
                     placeholder = "Senha"
                     type = "password"
                     icon = {FiLock}
+                    onChange = {e => setPassword(e.target.value)}
                 />
 
-                <Button  title="Entrar"/>
+                <Button  title="Entrar" onClick = { handleSignIn }/>
                 
 
                 <Link to="/register">
