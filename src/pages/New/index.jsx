eb114407a37648625a531e4123046670a6ea40b1
frederico <fredericonakajimanakajima@gmail.com>
2024-07-29 21:42:21 -0300

    Adicionar tag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -13,6 +13,9 @@ export function New(){
     const [links, setLinks] = useState([]);
     const [newLink, setNewLink] = useState("");
 
+    const [tags, setTags] = useState([]);
+    const [newTag, setNewTag] = useState("");
+
     function handleAddLink(){
         setLinks(prevState => [...prevState, newLink]);
         setNewLink("");
@@ -20,6 +23,11 @@ export function New(){
 
     function handleRemoveLink(deleted){
         setLinks(prevState => prevState.filter(link => link !== deleted));
+    }
+
+    function handleAddTag(){
+        setTags(prevState => [...prevState, newTag]);
+        setNewTag("");
     }
 
 
@@ -62,8 +70,23 @@ export function New(){
 
                 <Section title= "Marcadores">
                     <div className="tags">
-                        <NoteItem value= "react"/>
-                        <NoteItem isNew placeholder = "Nova tag"/>
+                        {
+                            tags.map((tag, index) => (
+                                <NoteItem
+                                    key={String(index)}
+                                    value= {tag}
+                                    onClick={() => { }}
+                                />
+                            ))
+                        }
+
+                        <NoteItem 
+                            isNew 
+                            placeholder="Nova tag"
+                            onChange={e => setNewTag(e.target.value)}
+                            value={ newTag }
+                            onClick={handleAddTag}
+                            />
                     </div>
                 </Section>
 
