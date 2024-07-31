ded552ef3a91942aed84d17c0282cf288cbb04fb
frederico <fredericonakajimanakajima@gmail.com>
2024-07-31 16:39:30 -0300

    Tag selecionada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,10 +10,19 @@ import { api } from '../../services/api'
 
 export function Home(){
     const [tags, setTags] = useState([]);
+    const [tagsSelected, setTagsSelected] = useState([]);
 
+    function handleTagsSelected(tagName){
+        const alreadySelected = tagsSelected.includes(tagName);
 
-
-
+        if(alreadySelected){
+            const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+            setTagsSelected(filteredTags);
+        }else{
+            setTagsSelected(prevState => [...prevState, tagName]);
+        }
+    }
+      
     useEffect(() => {
        async function fetchTags(){ 
             const response = await api.get("/tags");
@@ -34,27 +43,30 @@ export function Home(){
             <Menu>
                 <li>
                     <ButtonText 
-                    title= "Todos" 
-                    isActive
+                        title= "Todos" 
+                        onClick = {() => handleTagsSelected("all")}
+                        isActive = {tagsSelected.length === 0}
                     />
                 </li>
                {
                 tags && tags.map(tag => (  
                     <li key={String(tag.id)}>
                         <ButtonText 
-                        title= {tag.name}
+                            title= {tag.name}
+                            onClick = {() => handleTagsSelected(tag.name)}
+                            isActive = {tagsSelected.includes(tag.name)}
                         />
                     </li>
                 ))
                }
-          
-
-
             </Menu>
+          
 
             <Search>
                 <Input placeholder = "Pesquisar pelo título" />
             </Search>
+
+
 
             <Content>
                 <Section title = "Minhas notas">
