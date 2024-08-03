31ec260043006d7088d42df6b7e696e835482312
frederico <fredericonakajimanakajima@gmail.com>
2024-08-02 23:36:09 -0300

    Tratando rotas Indisponíveis

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from 'react-router-dom';
-
+import { Navigate } from 'react-router-dom';
 import { New } from '../pages/New';
 import { Home } from '../pages/Home';
 import { Profile } from '../pages/Profile';
@@ -12,6 +12,8 @@ export function AppRoutes(){
             <Route path='/new' element={<New/>}/>
             <Route path='/profile' element={<Profile/>}/>
             <Route path='/details/:id' element={<Details/>}/>
+
+            <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
     )
 }
