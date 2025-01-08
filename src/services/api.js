76a2039f91f66c2bd395a7a4d75531369c8dd34d
frederico <fredericonakajimanakajima@gmail.com>
2025-01-08 13:54:39 -0300

    api baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL:"https://backend-rocketnotes-cdl1.onrender.com"
+    baseURL:"https://backend-rocketnotes-t9mj.onrender.com"
 })
